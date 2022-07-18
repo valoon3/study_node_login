6732ac346b65580922e53fb6aa33d83231e24de0
valoon3 <valoon2@naver.com>
2022-07-18 21:05:52 +0900

    login dao 비동기적 처리 mysql promise 로 인한 정보 받아오는 방식이 java 와는 다름

--- src/routes/login/LoginDao.js
+++ src/routes/login/LoginDao.js
@@ -1,13 +1,24 @@
-const db = require('../../../mysql/index');
+const db = require('../../../mysql/index.js');
 
-function LoginDao() {
+// let a = db.query('select * from board');
+console.log(db);
 
-    this.selectUser = async () => {
-        let rows = await db.query('select * from user');
-        return rows;
-    }
-
-}
+// function LoginDao() {
+//
+//     // this.selectUser = async () => {
+//     //     let rows = await db.query('select * from user');
+//     //     return rows;
+//     // }
+//
+//     this.selectUser = async () => {
+//         let a = await db.query('select * from user');
+//
+//         return a;
+//     }
+//
+//
+//
+// }
 
 // const selectUser = () => {
 //     db.query('select * from user', (err, r) => {
@@ -18,10 +29,11 @@ function LoginDao() {
 //
 //     }
 // }
-let a = new LoginDao();
-a.selectUser()
-    .then((r) => {
-        console.log(r);
-    })
 
-module.exports = new LoginDao();
+// let a = new LoginDao();
+// a.selectUser()
+//     .then((r) => {
+//         console.log(r);
+//     })
+
+// module.exports = new LoginDao();
